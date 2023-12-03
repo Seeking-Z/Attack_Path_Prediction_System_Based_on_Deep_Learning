acf5dbf5bc097c424ba75d23f1e0f23bbfcb0d5d
Seeking <wqfxzhzh@126.com>
2023-12-03 12:53:03 +0800

    主要系统基本完成，还有网页等功能没做

--- install.py
+++ install.py
@@ -3,10 +3,7 @@ import os
 command_list = [
     'pip install -r requirements.txt',
     'docker pull ljyztzh/rgsqlserverimg',
-    'docker images',
-    'docker create --name rg_sqlserver -p 1433:1433 ljyztzh/rgsqlserverimg'
-    'docker ps -a'
-    'docker start rg_sqlserver'
+    'docker images'
 ]
 
 for command in command_list:
